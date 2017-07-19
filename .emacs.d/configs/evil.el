04b7585e93162942f09463ef64ee10b6edbd608d
Ricky Nelson <rickyn@socketwiz.com>
2017-07-18 22:54:21 -0400

    Update package handling, add markdown

--- .emacs.d/configs/evil.el
+++ .emacs.d/configs/evil.el
@@ -25,6 +25,8 @@
   (define-key evil-normal-state-map (kbd "SPC") my-leader-map)
   ;; disable this key sequence so we can use it in tide-mode
   (define-key evil-normal-state-map (kbd "M-.") nil)
+  ;; disable this key so we can use it in web-mode
+  (define-key evil-normal-state-map (kbd "C-n") nil)
   ;; disable this key so we can use it in yasnippets
   (define-key evil-motion-state-map (kbd "TAB") nil)
   )
