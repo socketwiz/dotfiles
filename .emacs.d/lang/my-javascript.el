2cc20629c9cf3b3a977403ee37caf838cbb9f02b
Ricky Nelson <rickyn@socketwiz.com>
2017-09-19 21:36:13 -0400

    Miscellaneous edits

--- .emacs.d/lang/my-javascript.el
+++ .emacs.d/lang/my-javascript.el
@@ -1,4 +1,14 @@
 
+(defun configure-web-mode-flycheck-checkers ()
+  ;; in order to have flycheck enabled in web-mode, add an entry to this
+  ;; cond that matches the web-mode engine/content-type/etc and returns the
+  ;; appropriate checker.
+  (-when-let (checker (cond
+                       ((string= web-mode-content-type "jsx")
+                        'javascript-eslint)))
+    (flycheck-mode)
+    (flycheck-select-checker checker)))
+
 (defun setup-tide-mode ()
   (interactive)
   (tide-setup)
@@ -7,50 +17,50 @@
 
 (defun my/init-javascript ()
   (use-package web-mode
-               :ensure t
-               :bind (("C-n" . web-mode-tag-match))
-               :mode (("\\.html?\\'" . web-mode)
-                      ("\\.js[x]?\\'" . web-mode)
-                      ("\\.css\\'" . web-mode)
-                      ("\\.scss\\'" . web-mode))
-               :config
-               (defadvice web-mode-highlight-part (around tweak-jsx activate)
-                          (if (equal web-mode-content-type "jsx")
-                            (let ((web-mode-enable-part-face nil))
-                              ad-do-it)
-                            ad-do-it))
+    :ensure t
+    :bind (("C-n" . web-mode-tag-match))
+    :mode (("\\.html?\\'" . web-mode)
+           ("\\.js[x]?\\'" . web-mode)
+           ("\\.css\\'" . web-mode)
+           ("\\.scss\\'" . web-mode))
+    :config
+    (defadvice web-mode-highlight-part (around tweak-jsx activate)
+      (if (equal web-mode-content-type "jsx")
+          (let ((web-mode-enable-part-face nil))
+            ad-do-it)
+        ad-do-it))
 
-               (defadvice web-mode-highlight-part (around tweak-jsx activate)
-                          (if (equal web-mode-content-type "js")
-                            (let ((web-mode-enable-part-face nil))
-                              ad-do-it)
-                            ad-do-it))
+    (defadvice web-mode-highlight-part (around tweak-jsx activate)
+      (if (equal web-mode-content-type "js")
+          (let ((web-mode-enable-part-face nil))
+            ad-do-it)
+        ad-do-it))
 
-               ;; disable lining up the args
-               (add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
-               (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
-               (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
-               (add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
-               :init
-               (setq web-mode-content-types-alist
-                     '(("jsx" . "\\.js[x]?\\'")
-                       ("javascript" . "\\.es6?\\'")))
+    ;; disable lining up the args
+    (add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
+    (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
+    (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
+    (add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
+    :init
+    (setq web-mode-content-types-alist
+          '(("jsx" . "\\.js[x]?\\'")
+            ("javascript" . "\\.es6?\\'")))
 
-               ;; disable auto-quoting
-               (setq web-mode-enable-auto-quoting nil)
-               ;; indent with 4 spaces
-               (setq-default indent-tabs-mode nil)
-               (setq web-mode-markup-indent-offset 4)
-               (setq web-mode-css-indent-offset 4)
-               (setq web-mode-code-indent-offset 4)
-               ;; automatically close tag
-               (setq web-mode-enable-auto-pairing t)
-               (setq web-mode-enable-css-colorization t)
-               ;; don't lineup element attributes
-               (setq web-mode-attr-indent-offset 4))
+    ;; disable auto-quoting
+    (setq web-mode-enable-auto-quoting nil)
+    ;; indent with 4 spaces
+    (setq-default indent-tabs-mode nil)
+    (setq web-mode-markup-indent-offset 4)
+    (setq web-mode-css-indent-offset 4)
+    (setq web-mode-code-indent-offset 4)
+    ;; automatically close tag
+    (setq web-mode-enable-auto-pairing t)
+    (setq web-mode-enable-css-colorization t)
+    ;; don't lineup element attributes
+    (setq web-mode-attr-indent-offset 4))
 
   (use-package skewer-mode
-               :ensure t)
+    :ensure t)
 
   (use-package tide
     :ensure t
@@ -61,7 +71,11 @@
     (add-hook 'web-mode-hook
               (lambda ()
                 (when (string-match-p "js[x]?" (file-name-extension buffer-file-name))
-                  (setup-tide-mode))))))
+                  (setup-tide-mode)))))
+
+  ;; use eslint with web-mode for js[x]? files
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+  (add-hook 'web-mode-hook #'configure-web-mode-flycheck-checkers))
 
 (provide 'my-javascript)
 
--- .emacs.d/lang/my-langs.el
+++ .emacs.d/lang/my-langs.el
@@ -1,9 +1,11 @@
 
+(require 'my-cpp)
 (require 'my-elisp)
 (require 'my-javascript)
 (require 'my-markdown)
 
 (defun my/init-langs ()
+  (my/init-cpp)
   (my/init-elisp)
   (my/init-javascript)
   (my/init-markdown))
