a3d9bfa0fc4128310741e1c14ec68e51744cbf22
Ricky Nelson <Ricky.Nelson@sophos.com>
2017-12-11 21:52:32 -0500

    use locally installed eslint

--- emacs/.emacs.d/lang/my-javascript.el
+++ emacs/.emacs.d/lang/my-javascript.el
@@ -7,6 +7,16 @@
                        ((string= web-mode-content-type "jsx")
                         'javascript-eslint)))
     (flycheck-mode)
+    ;; use the locally installed eslint
+    (let* ((root (locate-dominating-file
+                  (or (buffer-file-name) default-directory)
+                  "node_modules"))
+           (eslint (and root
+                        (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                          root))))
+      (when (and eslint (file-executable-p eslint))
+        (setq-local flycheck-javascript-eslint-executable eslint)))
+
     (flycheck-select-checker checker)))
 
 (defun setup-tide-mode ()
