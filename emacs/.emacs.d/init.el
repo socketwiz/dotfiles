083c026b4e74fb11919c76896b3bda6ef8d91bd5
Ricky Nelson <rickyn@socketwiz.com>
2017-12-15 17:03:10 -0500

    Set initial scratch mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -28,6 +28,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(initial-major-mode (quote emacs-lisp-mode))
  '(package-selected-packages
    (quote
     (dockerfile-mode smart-mode-line color-theme-modern swiper counsel counsel-projectile hydra ivy yasnippet winum tide smex web-mode google-this markdown-mode highlight-parentheses linum-relative exec-path-from-shell zerodark-theme which-key evil use-package skewer-mode pt popup paredit highlight numbers flyspell-popup flx evil-surround evil-magit diff-hl company platformio-mode highlight-numbers parent-mode evil-escape evil-anzu anzu proceed editorconfig diminish))))
