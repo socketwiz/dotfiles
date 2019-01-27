d4837da7ca9d016ba6eab990ac4788c3c3886d35
Ricky Nelson <rickyn@socketwiz.com>
2019-01-27 07:11:35 -0500

    update theme and rust-mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -20,13 +20,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("e3f648bb477a2e2332124f5ca8bd070e8624f152be6b4478668a69e5de7510ff" "a19265ef7ecc16ac4579abb1635fd4e3e1185dcacbc01b7a43cf7ad107c27ced" default)))
  '(initial-major-mode (quote emacs-lisp-mode))
  '(package-selected-packages
    (quote
-    (groovy-mode irony irony-eldoc flycheck-irony dracula-theme apropospriate-theme atomic-chrome evil evil-surround evil-escape evil-anzu undo-tree helm helm-config helm-projectile highlight-parentheses all-the-icons rainbow-mode flx-ido ido-completing-read+ helpful dockerfile-mode yasnippet winum tide web-mode google-this markdown-mode linum-relative exec-path-from-shell which-key use-package pt popup paredit highlight flyspell-popup flx magit diff-hl company platformio-mode highlight-numbers parent-mode anzu proceed editorconfig diminish scss-mode ghub))))
+    (racer rust-mode race flycheck-rust cargo groovy-mode irony irony-eldoc flycheck-irony dracula-theme atomic-chrome evil evil-surround evil-escape evil-anzu undo-tree helm helm-config helm-projectile highlight-parentheses all-the-icons rainbow-mode flx-ido ido-completing-read+ helpful dockerfile-mode yasnippet winum tide web-mode google-this markdown-mode linum-relative exec-path-from-shell which-key use-package pt popup paredit highlight flyspell-popup flx magit diff-hl company platformio-mode highlight-numbers parent-mode anzu proceed editorconfig diminish scss-mode ghub))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
