0df4b5b19d9abc3d19daf73d1daea9ade3811a50
Ricky Nelson <rickyn@socketwiz.com>
2019-03-19 13:14:54 -0400

    minor updates

---
+++ emacs/.emacs.d/transient/history.el
@@ -0,0 +1,8 @@
+((magit-commit nil)
+ (magit-diff
+  (("--" "resources/public/js/schedule.js")
+   "--no-ext-diff" "--stat")
+  (("--" "emacs/.emacs.d/settings.org")
+   "--no-ext-diff" "--stat"))
+ (magit-dispatch nil)
+ (magit-reset nil))
