02e67d30f51d3dc01213be90b4d32adc67278bbb
Ricky Nelson <hackerzol@pop-os.localdomain>
2022-04-12 18:47:11 -0400

    Minor updates
    * replacing dark theme
    * adding support for groovy (ugh)
    * minor environment updates

--- emacs/.emacs.d/lang/my-langs.el
+++ emacs/.emacs.d/lang/my-langs.el
@@ -3,11 +3,13 @@
 (require 'my-elisp)
 (require 'my-javascript)
 (require 'my-markdown)
+(require 'my-java)
 
 (defun my/init-langs ()
   (my/init-cpp)
   (my/init-elisp)
   (my/init-javascript)
-  (my/init-markdown))
+  (my/init-markdown)
+  (my/init-java))
 
 (provide 'my-langs)
