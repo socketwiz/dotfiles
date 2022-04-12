2f143d3a7dcfbe689fb4faa1a30066bd4cb45625
Ricky Nelson <rickyn@socketwiz.com>
2022-04-12 18:40:04 -0400

    Trying out smart-mode-line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -65,6 +65,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(package-selected-packages
    (quote
     (telephone-line telephone-line-config helm-descbinds yasnippet web-mode use-package tide smex markdown-mode magit linum-relative ivy-rich ivy-hydra helm-projectile helm-git-grep flycheck-color-mode-line evil counsel company alect-themes smex helm-smex helm-flx helm-fuzzier powerline helm-pt paredit which-key evil-surround diff-hl))))
