08516ea9a1a580b72b487418f6dc31816a870589
Ricky Nelson <rickyn@socketwiz.com>
2022-04-12 18:40:04 -0400

    removing irony

--- .emacs.d/configs/arduino.el
+++ .emacs.d/configs/arduino.el
@@ -1,33 +1,12 @@
 
-(use-package irony
-  :ensure t
-  :commands (irony-mode))
-
-(use-package company-irony
-  :ensure t)
-
-(use-package flycheck-irony
-  :ensure t
-  :init (add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
-
-(use-package irony-eldoc
-  :ensure t
-  :commands (irony-eldoc))
-
 (use-package platformio-mode
   :ensure t
   :commands (platformio-conditionally-enable)
   :mode (("\\.ino\\'" . c++-mode))
-  :init
-  (add-to-list 'company-backends 'company-irony)
+  :init)
 
-  ;; Setup irony for flycheck.
-  (add-hook 'flycheck-mode-hook 'flycheck-irony-setup))
-
-(defun irony-and-platformio-hook ()
-  (irony-mode)
-  (irony-eldoc)
+(defun platformio-hook ()
   (platformio-conditionally-enable))
 
-(add-hook 'c-mode-hook 'irony-and-platformio-hook)
-(add-hook 'c++-mode-hook 'irony-and-platformio-hook)
+(add-hook 'c-mode-hook 'platformio-hook)
+(add-hook 'c++-mode-hook 'platformio-hook)
