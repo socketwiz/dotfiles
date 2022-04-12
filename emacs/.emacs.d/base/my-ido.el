29dc9c5feafe30ee3060fea965c0013ab5beab89
Ricky Nelson <hackerzol@pop-os.localdomain>
2022-04-12 18:45:49 -0400

    Tame evil-mode, tweak recentf

--- emacs/.emacs.d/base/my-ido.el
+++ emacs/.emacs.d/base/my-ido.el
@@ -8,19 +8,28 @@
   (use-package ido-completing-read+
     :init
     (ido-ubiquitous-mode 1)
+    :config
+    (setq ido-cr+-max-items 3000)
     :defer t)
 
   (use-package flx-ido
     :init
     (flx-ido-mode 1)
-    (progn (setq ido-enable-flex-matching t))
     :defer t)
 
   (use-package recentf
     :config
-    (setq recentf-max-saved-items 50
-          recentf-max-menu-items 15)
-    (recentf-mode 1)))
+    (progn
+      (setq recentf-max-saved-items 10
+            recentf-max-menu-items 5
+            recentf-save-file (concat user-emacs-directory ".cache/recentf")
+            recentf-auto-cleanup 'never)
+      (recentf-mode 1)
+
+      (add-to-list 'recentf-exclude (expand-file-name package-user-dir))
+      (add-to-list 'recentf-exclude "COMMIT_EDITMSG\\'")
+      (add-to-list 'recentf-exclude (expand-file-name (concat user-emacs-directory ".cache/")))
+      (add-hook 'delete-terminal-functions (lambda (terminal) (recentf-save-list))))))
 
 (defun ido-recentf-open ()
   "Use `ido-completing-read' to \\[find-file] a recent file"
--- emacs/.emacs.d/base/my-projectile.el
+++ emacs/.emacs.d/base/my-projectile.el
@@ -7,15 +7,6 @@
                               (unless recentf-mode (recentf-mode) 
                                       (recentf-track-opened-file)))) 
 
-  (setq recentf-save-file (concat user-emacs-directory ".cache/recentf")
-        recentf-max-saved-items 1000
-        recentf-auto-cleanup 'never
-        recentf-auto-save-timer (run-with-idle-timer 600 t 'recentf-save-list))
-
-  ;; (add-to-list 'recentf-exclude (expand-file-name package-user-dir))
-  ;; (add-to-list 'recentf-exclude "COMMIT_EDITMSG\\'")
-  ;; (add-to-list 'recentf-exclude (expand-file-name (concat user-emacs-directory ".cache/")))
-
   (use-package projectile 
     :bind (("C-c p *" . projectile-pt))
     :diminish 'projectile-mode) 
