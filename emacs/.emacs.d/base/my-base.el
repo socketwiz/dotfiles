20f8c573e23295c2cc1e541fac87c0c6c36e903f
Ricky Nelson <hackerzol@pop-os.localdomain>
2022-04-12 18:45:49 -0400

    Removing ivy and paradox so I can get rid of hydra

--- emacs/.emacs.d/base/my-base.el
+++ emacs/.emacs.d/base/my-base.el
@@ -1,15 +1,13 @@
 
 (require 'my-evil)
-(require 'my-ivy)
+(require 'my-ido)
 (require 'my-projectile)
 (require 'my-emacs)
 (require 'my-git)
-(require 'my-hydra)
 
 (defun my/init-base ()
   (my/init-evil-base)
-  (my/init-hydra)
-  (my/init-ivy)
+  (my/init-ido)
   (my/init-projectile)
   (my/init-emacs)
   (my/init-git))
