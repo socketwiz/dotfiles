4c0a9736f3ddf78b237994b794c412b0da4477d7
Ricky Nelson <hackerzol@pop-os.localdomain>
2022-04-12 18:45:49 -0400

    Use ibuffer to manage buffers

--- emacs/.emacs.d/core/core-keybindings.el
+++ emacs/.emacs.d/core/core-keybindings.el
@@ -2,7 +2,7 @@
 (defun my/global-map () 
   (global-set-key (kbd "M-x") 'counsel-M-x)
   (global-set-key (kbd "C-s") 'swiper)
-  (global-set-key (kbd "C-x C-b") 'ivy-switch-buffer)
+  (global-set-key (kbd "C-x C-b") 'ibuffer)
   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
   (global-set-key (kbd "C-x C-r") 'counsel-recentf)
   (global-set-key (kbd "C-h b") 'counsel-descbinds))
