5f41d493251a143c1f961e8804c8ddb4924f541c
Ricky Nelson <rickyn@socketwiz.com>
2022-04-12 18:40:04 -0400

    Minor tweaks
    * setting up eldoc for lisp
    * fix startup message caused by smart-mode-line
    * remove unused packages
    * add elpa to pt ignore

--- .emacs.d/configs/completions.el
+++ .emacs.d/configs/completions.el
@@ -4,3 +4,13 @@
   :ensure t
   :config
   (add-hook 'after-init-hook 'global-company-mode))
+
+(use-package "eldoc"
+  :diminish eldoc-mode
+  :commands turn-on-eldoc-mode
+  :defer t
+  :init
+  (progn
+  (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+  (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+  (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)))
