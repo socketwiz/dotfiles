292a1b27c2255eb56a68a34265c794a6dea28e4b
Ricky Nelson <hackerzol@pop-os.localdomain>
2022-04-12 20:39:56 -0400

    Remove comments

--- emacs/.emacs.d/settings.el
+++ emacs/.emacs.d/settings.el
@@ -179,20 +179,12 @@ Signature (directory-files \"DIRECTORY\" &optional \"FULL\" \"MATCH\" \"NOSORT\"
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-c C-.") 'helpful-at-point)
-;;(global-set-key (kbd "C-c r") 'counsel-recentf)
 (global-set-key (kbd "C-h f") 'helpful-callable)
 (global-set-key (kbd "C-h k") 'helpful-key)
 (global-set-key (kbd "C-h v") 'helpful-variable)
-;;(global-set-key (kbd "C-x b") 'ivy-switch-buffer)
-;;(global-set-key (kbd "C-x d") 'counsel-dired)
-;;(global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-x C-e") 'pp-eval-last-sexp)
-;;(global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch)
-;;(global-set-key (kbd "M-i") 'counsel-imenu)
-;;(global-set-key (kbd "M-s s") 'swiper)
-;;(global-set-key (kbd "M-x") 'counsel-M-x)
 
 
 ;; * Core packages
