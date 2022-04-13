133e3bc26e51017ac22f36dc504c31e27b980e40
Ricky Nelson <rickyn@socketwiz.com>
2022-04-12 20:40:49 -0400

    Moar neovim config

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,4 +1,6 @@
 
+-- Shamlessly stole this `map` function from
+-- https://vonheikemen.github.io/devlog/tools/configuring-neovim-using-lua/
 local map = function(key)
   -- get the extra options
   local opts = {noremap = true}
@@ -21,6 +23,13 @@ map { 'n', '<Up>', '<Nop>' }
 map { 'n', '<Down>', '<Nop>' }
 map { 'n', '<Left>', '<Nop>' }
 map { 'n', '<Right>', '<Nop>' }
+-- Clear the search highlighting
+map { 'n', '<leader><space>', ':noh<cr>' }
+-- Copy/paste to the clipboard
+--map { 'n', 'y', '"+y' }
+--map { 'n', 'p', '"+p' }
+-- Format a json file with `jq`
+map { 'n', '<leader>j', ':%!jq .<cr>' }
 
 -- Telescope
 map { 'n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>' }
@@ -29,8 +38,23 @@ map { 'n', '<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<cr>' }
 map { 'n', '<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<cr>' }
 
 -- LSP
-map { 'n',  '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>' }
-map { 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>' }
-map { 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>' }
-map { 'n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>' }
+map { 'n',  '<space>e', '<cmd>lua vim.diagnostic.open_float()<cr>' }
+map { 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>' }
+map { 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>' }
+map { 'n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<cr>' }
+
+-- Nvim tree
+map { 'n', '<C-n>', '<cmd>NvimTreeToggle<cr>' }
+map { 'n', '<leader>r', '<cmd>NvimTreeRefresh<cr>' }
+map { 'n', '<C-x><C-j>', '<cmd>NvimTreeFindFile<cr>' }
+
+-- Which key
+local wk = require("which-key")
+local wk_mappings = {
+  q = { ':q<cr>', 'Quit' }
+}
+local wk_opts = {
+  prefix = '<leader>'
+}
+wk.register(wk_mappings, wk_opts)
 
