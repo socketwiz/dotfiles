d2afc128b571b9d444b5bed972c2d98f113c7c75
Ricky Nelson <hackerzol@pop-os.localdomain>
2022-04-12 20:39:04 -0400

    minor emacs and linux updates

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -10,7 +10,7 @@
  '(initial-major-mode (quote emacs-lisp-mode))
  '(package-selected-packages
    (quote
-    (evil lsp-mode rustic htmlize py-autopep8 elpy rg doom-modeline all-the-icons-dired ivy-hydra rjsx-mode js2-mode expand-region smartparens counsel-projectile counsel cider command-log-mode irony irony-eldoc flycheck-irony cyberpunk-theme atomic-chrome anzu undo-tree highlight-parentheses all-the-icons rainbow-mode flx helpful dockerfile-mode yasnippet tide web-mode markdown-mode exec-path-from-shell which-key use-package popup paredit highlight flyspell-popup magit magit-popup ghub diff-hl company platformio-mode highlight-numbers parent-mode proceed editorconfig diminish scss-mode))))
+    (evil-surround evil lsp-mode rustic htmlize py-autopep8 elpy rg doom-modeline all-the-icons-dired ivy-hydra rjsx-mode js2-mode expand-region smartparens counsel-projectile counsel cider command-log-mode irony irony-eldoc flycheck-irony cyberpunk-theme atomic-chrome anzu undo-tree highlight-parentheses all-the-icons rainbow-mode flx helpful dockerfile-mode yasnippet tide web-mode markdown-mode exec-path-from-shell which-key use-package popup paredit highlight flyspell-popup magit magit-popup ghub diff-hl company platformio-mode highlight-numbers parent-mode proceed editorconfig diminish scss-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
