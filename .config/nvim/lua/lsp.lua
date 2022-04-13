ae04fca5c5074f7f1e35cc6ea596d2f9282b0ffd
Ricky Nelson <rickyn@socketwiz.com>
2022-04-12 20:40:49 -0400

    Fixing js/ts for neovim

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -15,30 +15,30 @@ end
 local lsp_installer_servers = require('nvim-lsp-installer.servers')
 
 local servers = {
-    "rust_analyzer",
-    "tsserver",
+  "denols",
+  "rust_analyzer",
 }
 
 -- Loop through the servers listed above and set them up. If a server is
 -- not already installed, install it.
 for _, server_name in pairs(servers) do
-    local server_available, server = lsp_installer_servers.get_server(server_name)
-    if server_available then
-        server:on_ready(function ()
-            -- When this particular server is ready (i.e. when installation is finished or the server is already installed),
-            -- this function will be invoked. Make sure not to also use the "catch-all" lsp_installer.on_server_ready()
-            -- function to set up your servers, because by doing so you'd be setting up the same server twice.
-            local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-            local opts = {
-              capabilities = capabilities,
-              on_attach = on_attach
-            }
-            server:setup(opts)
-        end)
-        if not server:is_installed() then
-            -- Queue the server to be installed.
-            server:install()
-        end
+  local server_available, server = lsp_installer_servers.get_server(server_name)
+  if server_available then
+    server:on_ready(function ()
+      -- When this particular server is ready (i.e. when installation is finished or the server is already installed),
+      -- this function will be invoked. Make sure not to also use the "catch-all" lsp_installer.on_server_ready()
+      -- function to set up your servers, because by doing so you'd be setting up the same server twice.
+      local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+      local opts = {
+        capabilities = capabilities,
+        on_attach = on_attach
+      }
+      server:setup(opts)
+    end)
+    if not server:is_installed() then
+      -- Queue the server to be installed.
+      server:install()
     end
+  end
 end
 
