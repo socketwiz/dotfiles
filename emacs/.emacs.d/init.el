e5393e69d729f224744f9a8b3d4df19e2ee9b72c
Ricky Nelson <hackerzol@pop-os.localdomain>
2022-04-12 20:36:47 -0400

    trying new theme, organize a bit

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -23,7 +23,7 @@
  '(initial-major-mode (quote emacs-lisp-mode))
  '(package-selected-packages
    (quote
-    (racer rust-mode race flycheck-rust cargo irony irony-eldoc flycheck-irony dracula-theme atomic-chrome evil evil-surround evil-escape anzu evil-anzu undo-tree highlight-parentheses all-the-icons rainbow-mode flx flx-ido ido-completing-read+ helpful dockerfile-mode yasnippet tide web-mode markdown-mode linum-relative exec-path-from-shell which-key use-package pt popup paredit highlight flyspell-popup magit ghub diff-hl company platformio-mode highlight-numbers parent-mode proceed editorconfig diminish scss-mode))))
+    (racer rust-mode race flycheck-rust cargo irony irony-eldoc flycheck-irony dracula-theme atomic-chrome evil evil-surround evil-escape anzu evil-anzu undo-tree highlight-parentheses all-the-icons rainbow-mode flx flx-ido ido-completing-read+ helpful dockerfile-mode yasnippet tide web-mode markdown-mode exec-path-from-shell which-key use-package pt popup paredit highlight flyspell-popup magit ghub diff-hl company platformio-mode highlight-numbers parent-mode proceed editorconfig diminish scss-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
