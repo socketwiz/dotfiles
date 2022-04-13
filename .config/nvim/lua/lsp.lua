4bbf5381b2a7f629525a0def7cdd1776f1009706
Ricky Nelson <rickyn@socketwiz.com>
2022-04-12 20:40:49 -0400

    Setting deno up for lsp

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -12,33 +12,25 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
 end
 
-local lsp_installer_servers = require('nvim-lsp-installer.servers')
+local nvim_lsp = require('lspconfig')
 
-local servers = {
-  "denols",
-  "rust_analyzer",
+-- Enable bash analyzer
+-- npm i -g bash-language-server
+nvim_lsp.bashls.setup {}
+
+-- Enable html/css analyzer
+-- npm i -g vscode-langservers-extracted
+nvim_lsp.cssls.setup {}
+nvim_lsp.html.setup {}
+--
+-- Enable javascript/typescript analyzer
+nvim_lsp.denols.setup {
+  on_attach = on_attach,
+  init_options = {
+    lint = true,
+  },
 }
 
--- Loop through the servers listed above and set them up. If a server is
--- not already installed, install it.
-for _, server_name in pairs(servers) do
-  local server_available, server = lsp_installer_servers.get_server(server_name)
-  if server_available then
-    server:on_ready(function ()
-      -- When this particular server is ready (i.e. when installation is finished or the server is already installed),
-      -- this function will be invoked. Make sure not to also use the "catch-all" lsp_installer.on_server_ready()
-      -- function to set up your servers, because by doing so you'd be setting up the same server twice.
-      local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-      local opts = {
-        capabilities = capabilities,
-        on_attach = on_attach
-      }
-      server:setup(opts)
-    end)
-    if not server:is_installed() then
-      -- Queue the server to be installed.
-      server:install()
-    end
-  end
-end
+-- Enable rust_analyzer
+nvim_lsp.rust_analyzer.setup {}
 
