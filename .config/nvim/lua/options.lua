101724155ee563aafab68a565cf892b2db951c06
Ricky Nelson <rickyn@socketwiz.com>
2022-04-12 20:40:49 -0400

    Moar neovim config

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,15 @@
+
+local opt = vim.opt  -- to set options
+
+opt.expandtab = true                -- Use spaces instead of tabs
+opt.hidden = true                   -- Enable background buffers
+opt.ignorecase = true               -- Ignore case
+opt.joinspaces = false              -- No double spaces with join
+opt.list = true                     -- Show some invisible characters
+opt.number = true                   -- Show line numbers
+opt.relativenumber = true           -- Relative line numbers
+opt.scrolloff = 3                   -- Lines of context
+opt.shiftwidth = 2                  -- Size of an indent
+opt.smartcase = true                -- Do not ignore case with capitals
+opt.tabstop = 2                     -- Number of spaces tabs count for
+opt.termguicolors = true            -- True color support
