d1cc72e667122d256d88529d16ac9a255dbe07a0
Ricky Nelson <hackerzol@pop-os.localdomain>
2022-04-12 20:36:48 -0400

    removing unused files

--- emacs/.emacs.d/transient/history.el
+++
@@ -1,8 +0,0 @@
-((magit-commit nil)
- (magit-diff
-  (("--" "resources/public/js/schedule.js")
-   "--no-ext-diff" "--stat")
-  (("--" "emacs/.emacs.d/settings.org")
-   "--no-ext-diff" "--stat"))
- (magit-dispatch nil)
- (magit-reset nil))
