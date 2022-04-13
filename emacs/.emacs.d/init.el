c87705a20d91dce7551e6dcbddd7dc916b71689f
Ricky Nelson <hackerzol@pop-os.localdomain>
2022-04-12 20:39:56 -0400

    Add rust-mode to installed plugin list

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -18,7 +18,7 @@
  '(initial-major-mode (quote emacs-lisp-mode))
  '(package-selected-packages
    (quote
-    (all-the-icons all-the-icons-dired anzu atomic-chrome cider command-log-mode company company-lsp dashboard diff-hl diminish dockerfile-mode doom-modeline doom-themes editorconfig elpy exec-path-from-shell expand-region evil evil-surround flycheck-irony flyspell-popup flx ghub helpful highlight highlight-numbers highlight-parentheses htmlize ido-completing-read+ ido-vertical-mode irony irony-eldoc js2-mode json-mode lsp-mode lsp-ui magit magit-popup markdown-mode org-agenda paredit parent-mode popup prescient proceed py-autopep8 rainbow-mode rg rjsx-mode scss-mode smartparens tide undo-tree use-package web-mode which-key yasnippet))))
+    (all-the-icons all-the-icons-dired anzu atomic-chrome cider command-log-mode company company-lsp dashboard diff-hl diminish dockerfile-mode doom-modeline doom-themes editorconfig elpy exec-path-from-shell expand-region evil evil-surround flycheck-irony flyspell-popup flx ghub helpful highlight highlight-numbers highlight-parentheses htmlize ido-completing-read+ ido-vertical-mode irony irony-eldoc js2-mode json-mode lsp-mode lsp-ui magit magit-popup markdown-mode org-agenda paredit parent-mode popup prescient proceed py-autopep8 rainbow-mode rg rjsx-mode rust-mode scss-mode smartparens tide undo-tree use-package web-mode which-key yasnippet))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
