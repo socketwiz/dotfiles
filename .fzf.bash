0f741d3c4be5f274a01df9004ae147086b6103b5
Ricky Nelson <rickyn@socketwiz.com>
2022-04-12 20:40:48 -0400

    Minor environment updates

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */home/hackerzol/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/hackerzol/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/hackerzol/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
+source "/home/hackerzol/.fzf/shell/key-bindings.bash"
