25805450d7addd8e715ee9a1d20ac2a1d3cfdd35
Ricky Nelson <hackerzol@pop-os.localdomain>
2022-04-12 20:36:47 -0400

    update emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -23,7 +23,7 @@
  '(initial-major-mode (quote emacs-lisp-mode))
  '(package-selected-packages
    (quote
-    (racer rust-mode race flycheck-rust cargo groovy-mode irony irony-eldoc flycheck-irony dracula-theme atomic-chrome evil evil-surround evil-escape evil-anzu undo-tree helm helm-config helm-projectile highlight-parentheses all-the-icons rainbow-mode flx-ido ido-completing-read+ helpful dockerfile-mode yasnippet winum tide web-mode google-this markdown-mode linum-relative exec-path-from-shell which-key use-package pt popup paredit highlight flyspell-popup flx magit diff-hl company platformio-mode highlight-numbers parent-mode anzu proceed editorconfig diminish scss-mode ghub))))
+    (racer rust-mode race flycheck-rust cargo groovy-mode irony irony-eldoc flycheck-irony dracula-theme atomic-chrome evil evil-surround evil-escape evil-anzu undo-tree helm helm-config helm-projectile highlight-parentheses all-the-icons rainbow-mode flx-ido ido-completing-read+ helpful dockerfile-mode yasnippet tide web-mode google-this markdown-mode linum-relative exec-path-from-shell which-key use-package pt popup paredit highlight flyspell-popup flx magit diff-hl company platformio-mode highlight-numbers parent-mode anzu proceed editorconfig diminish scss-mode ghub))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
