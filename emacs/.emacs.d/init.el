2ed4d2667e5643e0c52d2db842a060c380c1a15f
Ricky Nelson <hackerzol@pop-os.localdomain>
2022-04-12 20:39:56 -0400

    Minor javascript changes for emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -14,10 +14,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(initial-major-mode (quote emacs-lisp-mode))
+ '(initial-major-mode 'emacs-lisp-mode)
  '(package-selected-packages
-   (quote
-    (all-the-icons all-the-icons-dired anzu atomic-chrome cider command-log-mode company company-lsp dashboard diff-hl diminish dockerfile-mode doom-modeline doom-themes editorconfig elpy exec-path-from-shell expand-region evil evil-surround flyspell-popup flx ghub helpful highlight highlight-numbers highlight-parentheses htmlize ido-completing-read+ ido-vertical-mode irony irony-eldoc js2-mode json-mode lsp-mode lsp-ui magit magit-popup markdown-mode org-agenda paredit parent-mode popup prescient proceed projectile py-autopep8 rainbow-mode rg rjsx-mode rust-mode scss-mode smartparens undo-tree use-package web-mode which-key yasnippet))))
+   '(all-the-icons all-the-icons-dired anzu atomic-chrome cider command-log-mode company company-lsp dashboard diff-hl diminish dockerfile-mode doom-modeline doom-themes editorconfig elpy exec-path-from-shell expand-region evil evil-surround flyspell-popup flx ghub helpful highlight highlight-numbers highlight-parentheses htmlize ido-completing-read+ ido-vertical-mode irony irony-eldoc js2-mode json-mode lsp-mode lsp-ui magit magit-popup markdown-mode org-agenda paredit parent-mode popup prescient proceed projectile py-autopep8 rainbow-mode rg rjsx-mode rust-mode scss-mode smartparens tide undo-tree use-package web-mode which-key yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
