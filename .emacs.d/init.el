8ff88cb54f12d199130700a33237b1792ec95aea
Ricky Nelson <rickyn@socketwiz.com>
2022-07-18 06:23:25 -0400

    cleanup emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,7 +11,6 @@
 (load "~/.emacs.d/packages.el")
 (load "~/.emacs.d/javascript.el")
 (load "~/.emacs.d/rust.el")
-(load "~/.emacs.d/python.el")
 (load "~/.emacs.d/shell.el")
 
 (provide 'init)
@@ -24,7 +23,7 @@
  ;; If there is more than one, they won't work right.
  '(gnutls-algorithm-priority "normal:-vers-tls1.3")
  '(package-selected-packages
-   '(all-the-icons all-the-icons-dired atomic-chrome avy bind-key command-log-mode dap diff-hl diminish dockerfile-mode doom-modeline doom-themes editorconfig eglot elpy embark embark-consult exec-path-from-shell expand-region evil evil-surround flymake-eslint flymake-python-pyflakes flymake-shellcheck helpful indium jedi marginalia markdown-mode mu4e orderless org-mode org-bullets pipenv prettier-js rainbow-mode rg rust-mode scss-mode smartparens swiper tide tree-sitter-langs tree-sitter undo-tree use-package vertico web-mode which-key yasnippet yaml-mode)))
+   '(all-the-icons all-the-icons-dired bind-key command-log-mode consult diff-hl diminish dockerfile-mode doom-modeline doom-themes editorconfig eglot exec-path-from-shell evil evil-surround flymake-eslint flymake-shellcheck helpful indium marginalia markdown-mode orderless org-mode org-bullets prettier-js rainbow-mode rg rust-mode scss-mode smartparens swiper tide tree-sitter-langs tree-sitter undo-tree use-package vertico web-mode which-key yasnippet yaml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
