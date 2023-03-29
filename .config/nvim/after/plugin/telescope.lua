0b8eb5e8df898cd8f353d70f7538ab03e1589f79
Ricky Nelson <rickyn@getpicnic.com>
2023-03-29 11:21:15 -0400

    tweaking neovim telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,11 @@
 local actions = require('telescope.actions')
+local builtin = require('telescope.builtin')
 
 require('telescope').setup({
     defaults = {
+        layout_strategy = 'vertical',
+        layout_config = { height = 0.95 },
+
         mappings = {
             i = {
                 -- close telescope with a single ESC
@@ -10,4 +14,3 @@ require('telescope').setup({
         },
     },
 })
-
