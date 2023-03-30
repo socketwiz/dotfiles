eceb8cb0d3c7949f6fe8c3b925b085c45b6160de
Ricky Nelson <rickyn@socketwiz.com>
2023-03-30 18:06:09 -0400

    installed a lua formatter

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -1,21 +1,19 @@
-
-require('nvim-tree').setup {
-  disable_netrw = true,
-  hijack_netrw  = true,
-  update_focused_file = {
-    enable = true,
-    update_cwd  = true
-  },
-  view = {
-    mappings = {
-      list = {
-      { key = "g", action = "refresh" },
-      { key = "+", action = "create" },
-      { key = "x", action = "remove" },
-      { key = "R", action = "rename" },
-      { key = "^", action = "dir_up" },
-      }
-    },
-  },
-}
-
+require("nvim-tree").setup({
+	disable_netrw = true,
+	hijack_netrw = true,
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
+	view = {
+		mappings = {
+			list = {
+				{ key = "g", action = "refresh" },
+				{ key = "+", action = "create" },
+				{ key = "x", action = "remove" },
+				{ key = "R", action = "rename" },
+				{ key = "^", action = "dir_up" },
+			},
+		},
+	},
+})
--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,38 +1,44 @@
-
-vim.keymap.set('n', '<Up>', '<Nop>')
-vim.keymap.set('n', '<Down>', '<Nop>')
-vim.keymap.set('n', '<Left>', '<Nop>')
-vim.keymap.set('n', '<Right>', '<Nop>')
+vim.keymap.set("n", "<Up>", "<Nop>")
+vim.keymap.set("n", "<Down>", "<Nop>")
+vim.keymap.set("n", "<Left>", "<Nop>")
+vim.keymap.set("n", "<Right>", "<Nop>")
 -- Clear the search highlighting
-vim.keymap.set('n', '<leader><space>', ':noh<cr>')
+vim.keymap.set("n", "<leader><space>", ":noh<cr>")
 
 -- Nvim tree
-vim.keymap.set('n', '<C-x><C-j>', '<cmd>NvimTreeFindFile<cr>')
+vim.keymap.set("n", "<C-x><C-j>", "<cmd>NvimTreeFindFile<cr>")
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 cmp.setup({
-  mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }
+	mapping = {
+		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+		-- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+		["<C-y>"] = cmp.config.disable,
+		["<C-e>"] = cmp.mapping({
+			i = cmp.mapping.abort(),
+			c = cmp.mapping.close(),
+		}),
+		-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	},
 })
 
-local telescope = require('telescope.builtin')
+local telescope = require("telescope.builtin")
 local path_display = {
-  path_display = { shorten = 3 }
+	path_display = { shorten = 3 },
 }
-vim.keymap.set('n', '<leader>fb', (function() telescope.buffers(path_display) end), {})
-vim.keymap.set('n', '<leader>ff', (function() telescope.find_files(path_display) end), {})
-vim.keymap.set('n', '<C-p>', (function() telescope.git_files(path_display) end), {})
-vim.keymap.set('n', '<leader>fg', function()
+vim.keymap.set("n", "<leader>fb", function()
+	telescope.buffers(path_display)
+end, {})
+vim.keymap.set("n", "<leader>ff", function()
+	telescope.find_files(path_display)
+end, {})
+vim.keymap.set("n", "<C-p>", function()
+	telescope.git_files(path_display)
+end, {})
+vim.keymap.set("n", "<leader>fg", function()
 	telescope.grep_string({ search = vim.fn.input("Grep > ") })
 end)
-vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
-
+vim.keymap.set("n", "<leader>fh", telescope.help_tags, {})
