9b887456babd8b8c62585a6023b4076fa651b0c4
Ricky Nelson <rickyn@socketwiz.com>
2023-04-01 14:46:23 -0400

    even moar neovim config

--- .config/nvim/after/plugin/copilot.lua
+++ .config/nvim/after/plugin/copilot.lua
@@ -1,41 +1,10 @@
-require('copilot').setup({
-  panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<CR>",
-      refresh = "gr",
-      open = "<M-CR>"
-    },
-    layout = {
-      position = "bottom", -- | top | left | right
-      ratio = 0.4
-    },
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = false,
-    debounce = 75,
-    keymap = {
-      accept = "<M-l>",
-      accept_word = false,
-      accept_line = false,
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
-    },
-  },
-  filetypes = {
-    yaml = false,
-    markdown = false,
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    ["."] = false,
-  },
-  copilot_node_command = 'node', -- Node.js version must be > 16.x
-  server_opts_overrides = {},
-})
+vim.g.copilot_filetypes = {
+  markdown = false,
+  xml = false,
+}
+
+vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+vim.keymap.set('i', '<C-i>', '<Plug>(copilot-next)', {})
+vim.keymap.set('i', '<C-k>', '<Plug>(copilot-previous)', {})
+vim.keymap.set('i', '<M-]>', '<Plug>(copilot-dismiss)', {})
+vim.g.copilot_no_tab_map = true
--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,55 +1,55 @@
 local lsp = require("lsp-zero").preset({
-	name = "recommended",
+  name = "recommended",
 })
 
 lsp.ensure_installed({
-	"eslint",
-	"rust_analyzer",
-	"tsserver",
+  "eslint",
+  "rust_analyzer",
+  "tsserver",
 })
 
--- lsp.format_on_save({
--- 	servers = {
--- 		["eslint"] = { "javascript" },
---     [ "lua_ls"] = {"lua"},
--- 		["tsserver"] = { "typescript" },
--- 		["rust_analyzer"] = { "rust" },
--- 	},
--- })
+lsp.format_on_save({
+  servers = {
+    ["eslint"] = { "javascript" },
+    ["lua_ls"] = { "lua" },
+    ["tsserver"] = { "typescript" },
+    ["rust_analyzer"] = { "rust" },
+  },
+})
 
 lsp.on_attach(function(client, bufnr)
-	local opts = { buffer = bufnr, remap = false }
+  local opts = { buffer = bufnr, remap = false }
 
-	vim.keymap.set("n", "gd", function()
-		vim.lsp.buf.definition()
-	end, opts)
-	vim.keymap.set("n", "K", function()
-		vim.lsp.buf.hover()
-	end, opts)
-	vim.keymap.set("n", "<leader>vws", function()
-		vim.lsp.buf.workspace_symbol()
-	end, opts)
-	vim.keymap.set("n", "<leader>vd", function()
-		vim.diagnostic.open_float()
-	end, opts)
-	vim.keymap.set("n", "[d", function()
-		vim.diagnostic.goto_next()
-	end, opts)
-	vim.keymap.set("n", "]d", function()
-		vim.diagnostic.goto_prev()
-	end, opts)
-	vim.keymap.set("n", "<leader>vca", function()
-		vim.lsp.buf.code_action()
-	end, opts)
-	vim.keymap.set("n", "<leader>vrr", function()
-		vim.lsp.buf.references()
-	end, opts)
-	vim.keymap.set("n", "<leader>vrn", function()
-		vim.lsp.buf.rename()
-	end, opts)
-	vim.keymap.set("i", "<C-h>", function()
-		vim.lsp.buf.signature_help()
-	end, opts)
+  vim.keymap.set("n", "gd", function()
+    vim.lsp.buf.definition()
+  end, opts)
+  vim.keymap.set("n", "K", function()
+    vim.lsp.buf.hover()
+  end, opts)
+  vim.keymap.set("n", "<leader>ws", function()
+    vim.lsp.buf.workspace_symbol()
+  end, opts)
+  vim.keymap.set("n", "<leader>d", function()
+    vim.diagnostic.open_float()
+  end, opts)
+  vim.keymap.set("n", "[d", function()
+    vim.diagnostic.goto_next()
+  end, opts)
+  vim.keymap.set("n", "]d", function()
+    vim.diagnostic.goto_prev()
+  end, opts)
+  vim.keymap.set("n", "<leader>ca", function()
+    vim.lsp.buf.code_action()
+  end, opts)
+  vim.keymap.set("n", "<leader>crf", function()
+    vim.lsp.buf.references()
+  end, opts)
+  vim.keymap.set("n", "<leader>crn", function()
+    vim.lsp.buf.rename()
+  end, opts)
+  vim.keymap.set("n", "<C-k>", function()
+    vim.lsp.buf.signature_help()
+  end, opts)
 end)
 
 lsp.setup()
