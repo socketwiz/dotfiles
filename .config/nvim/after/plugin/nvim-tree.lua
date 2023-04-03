bf6a5600d2bdacfbecc7bfa221eb4ad24f5c4374
Ricky Nelson <rickyn@getpicnic.com>
2023-04-03 12:58:16 -0400

    i3 vim tweaks

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -1,19 +1,24 @@
 require("nvim-tree").setup({
-	disable_netrw = true,
-	hijack_netrw = true,
-	update_focused_file = {
-		enable = true,
-		update_cwd = true,
-	},
-	view = {
-		mappings = {
-			list = {
-				{ key = "g", action = "refresh" },
-				{ key = "+", action = "create" },
-				{ key = "x", action = "remove" },
-				{ key = "R", action = "rename" },
-				{ key = "^", action = "dir_up" },
-			},
-		},
-	},
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
+  },
+  disable_netrw = true,
+  hijack_netrw = true,
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
+  view = {
+    mappings = {
+      list = {
+        { key = "g", action = "refresh" },
+        { key = "+", action = "create" },
+        { key = "x", action = "remove" },
+        { key = "R", action = "rename" },
+        { key = "^", action = "dir_up" },
+      },
+    },
+  },
 })
