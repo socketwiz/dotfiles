c5deb94d31e2abadc37a4e9abe6dca41c9fe5441
Ricky Nelson <rickyn@socketwiz.com>
2023-04-11 08:28:13 -0400

    Fix neoviom grep keybind
    * grep now pre-populates word under cursor

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -10,35 +10,37 @@ vim.keymap.set("n", "<C-x><C-j>", "<cmd>NvimTreeFindFile<cr>")
 
 local cmp = require("cmp")
 cmp.setup({
-	mapping = {
-		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		-- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-		["<C-y>"] = cmp.config.disable,
-		["<C-e>"] = cmp.mapping({
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.close(),
-		}),
-		-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-	},
+  mapping = {
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ["<C-y>"] = cmp.config.disable,
+    ["<C-e>"] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  },
 })
 
 local telescope = require("telescope.builtin")
 local path_display = {
-	path_display = { shorten = 3 },
+  path_display = { shorten = 3 },
 }
 vim.keymap.set("n", "<leader>fb", function()
-	telescope.buffers(path_display)
+  telescope.buffers(path_display)
 end, {})
 vim.keymap.set("n", "<leader>ff", function()
-	telescope.find_files(path_display)
+  telescope.find_files(path_display)
 end, {})
 vim.keymap.set("n", "<C-p>", function()
-	telescope.git_files(path_display)
+  telescope.git_files(path_display)
 end, {})
 vim.keymap.set("n", "<leader>fg", function()
-	telescope.grep_string({ search = vim.fn.input("Grep > ") })
+  local current_word = vim.call('expand', '<cword>')
+
+  telescope.grep_string({ search = vim.fn.input("Grep > ", current_word) })
 end)
 vim.keymap.set("n", "<leader>h", telescope.help_tags, {})
