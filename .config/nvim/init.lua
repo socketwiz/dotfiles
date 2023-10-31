8397901055eed983302d5b9d44a02f0a3fa455dd
Ricky Nelson <rickyn@socketwiz.com>
2023-10-31 16:31:58 -0400

    Updating neovim package manager
    * moving from packer to lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,17 @@
-require "core"
-
--- Bootstrap Packer if it is not already available
-local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-    install_path })
+-- Bootstrap Lazy if it is not already available
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
+require "core"
 require('plugins')
 require('keymaps')
