f97058893608b0b6a471e097bc6e346b476a2a82
Ricky Nelson <rickyn@socketwiz.com>
2024-06-04 09:13:57 -0400

    more emacs cleanup

--- .emacs.d/javascript.el
+++ .emacs.d/javascript.el
@@ -1,9 +1,9 @@
-;;; settings.el --- Language JavaScript/TypeScript
+;;; javascript.el --- Language JavaScript/TypeScript
 
 ;; Author: Ricky Nelson <rickyn@socketwiz.com>
 
 ;;; Commentary:
-;; Settings the JavaScript/TypeScript, HTML, and CSS languages for frontend development
+;; Setting the JavaScript/TypeScript, HTML, and CSS languages for frontend development
 
 ;;; Code:
 ;; To enable the lsp backend:
@@ -25,25 +25,6 @@
 ;;  "indentSize": 2,
 ;;  "tabSize": 2
 ;; }
-(defun configure-web-mode ()
-  "Need an extra check here to setup JSX."
-  (message "conifgure-web-mode")
-  (if (equal web-mode-content-type "javascript")
-      (funcall (lambda ()
-        (message "web-mode-content-type=javascript")
-        (web-mode-set-content-type "jsx")
-        (message "now set to: %s" web-mode-content-type))))
-  (configure-mode))
-
-(defun configure-mode ()
-  "Setup linters, yas and such."
-  (eglot-ensure)
-  (yas-minor-mode)
-  (smartparens-mode)
-  (show-smartparens-mode)
-  (prettier-js-mode)
-  (setq js-indent-level config-indent-web-mode-spaces)
-  (setq eldoc-documentation-strategy #'eldoc-documentation-compose))
 
 ;; Flymake eslint backend
 (use-package flymake-eslint
@@ -51,73 +32,21 @@
 
 (use-package prettier-js)
 
-(add-hook 'css-ts-mode-hook 'configure-mode)
-(add-hook 'js-ts-mode-hook 'configure-mode)
-(add-hook 'typescript-ts-mode-hook 'configure-mode)
-(add-hook 'web-mode-hook 'configure-web-mode)
-
-;; treesit-install-language-grammar
-;;(add-to-list 'auto-mode-alist '("\\.css\\'" . css-ts-mode))
-;;(add-to-list 'auto-mode-alist '("\\.scss\\'" . css-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.json\\'" . json-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-ts-mode))
-
-;; Ensure the grammar is loaded for JavaScript mode
-(add-hook 'js-mode-hook
-          (lambda ()
-            (tree-sitter-mode)
-            (tree-sitter-hl-mode)))
-;; JSON grammar
-(add-hook 'json-mode-hook
-          (lambda ()
-            (tree-sitter-mode)
-            (tree-sitter-hl-mode)))
-
-;; * Language HTML
-(defun web-mode-init ()
-  "Setup yas when in \"web-mode\"."
-  (interactive)
-  (yas-minor-mode)
-  ;; disable auto-pairing just in web-mode so in django templates
-  ;; you can do {% %} without it becoming {% %}}
-  (electric-pair-mode -1))
-
-;; Major mode for editing web templates
-(use-package web-mode
-  :if config-enable-web-mode
-  :mode (("\\.html?\\'" . web-mode))
-  :config
-  (defadvice web-mode-highlight-part (around tweak-jsx activate)
-    (if (equal web-mode-content-type "js")
-        (let ((web-mode-enable-part-face nil))
-          ad-do-it)
-      ad-do-it))
-
-  ;; Disable lining up the args
-  (add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
-  (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
-  (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
-  (add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
-  :init
-  (setq-default indent-tabs-mode nil)
-  (add-hook 'web-mode-hook 'web-mode-init)
+(use-package js-base-mode
+  :defer 't
+  :ensure js
   :custom
-  (web-mode-engines-alist
-   '(("django" . "\\.html\\'")))
+  (js-indent-level 2)
+  :config
+  (unbind-key "M-." js-base-mode-map))
 
-  ;; Disable auto-quoting
-  (web-mode-enable-auto-quoting nil)
-  (web-mode-markup-indent-offset config-indent-web-mode-spaces)
-  (web-mode-css-indent-offset config-indent-web-mode-spaces)
-  (web-mode-code-indent-offset config-indent-web-mode-spaces)
-  ;; Don't lineup element attributes
-  (web-mode-attr-indent-offset config-indent-web-mode-spaces)
-  ;; Automatically close tag
-  (web-mode-enable-auto-pairing t)
-  (web-mode-enable-css-colorization t))
+(use-package typescript-ts-mode
+  :ensure typescript-ts-mode
+  :defer 't
+  :custom
+  (typescript-indent-level 2)
+  :config
+  (unbind-key "M-." typescript-ts-base-mode-map))
 
 (use-package indium
   :init
