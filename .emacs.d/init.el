6f2edd5a0c96a13116ec63151d884397f0e8f91b
Ricky Nelson <rickyn@socketwiz.com>
2024-06-10 10:05:13 -0400

    Add missing emacs package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,7 +24,7 @@
  ;; If there is more than one, they won't work right.
  '(gnutls-algorithm-priority "normal:-vers-tls1.3")
  '(package-selected-packages
-   '(all-the-icons all-the-icons-dired bind-key casual-info command-log-mode consult copilot diff-hl diminish dockerfile-mode doom-modeline doom-themes editorconfig exec-path-from-shell evil evil-surround flymake-eslint flymake-shellcheck graphql-mode helpful indium marginalia markdown-mode orderless org-bullets org-mode prettier-js quelpa quelpa-use-package rainbow-mode rg smartparens swiper terraform-mode tree-sitter tree-sitter-langs treesit treesitter undo-tree vertico web-mode which-key yaml-mode yasnippet)))
+   '(all-the-icons all-the-icons-dired bind-key casual-info command-log-mode consult copilot diff-hl diminish dockerfile-mode doom-modeline doom-themes editorconfig exec-path-from-shell evil evil-surround flymake-eslint flymake-shellcheck graphql-mode helpful indium marginalia markdown-mode orderless org-bullets org-mode paredit prettier-js quelpa quelpa-use-package rainbow-mode rg smartparens swiper terraform-mode tree-sitter tree-sitter-langs treesit treesitter undo-tree vertico web-mode which-key yaml-mode yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
