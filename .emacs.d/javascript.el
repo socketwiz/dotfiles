b176df97095dbc2d89849c3f95084b0abc0054d7
Ricky Nelson <rickyn@socketwiz.com>
2024-06-10 10:03:55 -0400

    Adding vue support to emacs

--- .emacs.d/javascript.el
+++ .emacs.d/javascript.el
@@ -54,6 +54,67 @@
   (unbind-key "M-." typescript-ts-base-mode-map)
   :hook (typescript-ts-mode . enable-prettier-js-mode))
 
+(defun setup-vue-mode ()
+  "Custom configurations for editing Vue files with web-mode."
+  (setq web-mode-content-type "vue")
+  ;; Set up embedded JS mode
+  (add-to-list 'web-mode-engines-alist '("vue" . "\\.vue\\'"))
+  (web-mode-set-content-type "vue")
+  ;; Use js-mode for JavaScript sections
+  (setq-local web-mode-script-padding 0)
+  (setq-local web-mode-style-padding 0)
+  (setq-local web-mode-block-padding 0)
+
+  ;; Enable prettier-js-mode for web-mode
+  (enable-prettier-js-mode))
+
+;; * Language HTML
+(defun web-mode-init ()
+  "Setup yas when in \"web-mode\"."
+  (interactive)
+  (yas-minor-mode)
+  ;; disable auto-pairing just in web-mode so in django templates
+  ;; you can do {% %} without it becoming {% %}}
+  (electric-pair-mode -1))
+
+;; Major mode for editing web templates
+(use-package web-mode
+  :if config-enable-web-mode
+  :mode (("\\.html?\\'" . web-mode)
+         ("\\.vue\\'" . web-mode))
+  :config
+  (defadvice web-mode-highlight-part (around tweak-jsx activate)
+    (if (equal web-mode-content-type "js")
+        (let ((web-mode-enable-part-face nil))
+          ad-do-it)
+      ad-do-it))
+
+  ;; Disable lining up the args
+  (add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
+  :init
+  (setq-default indent-tabs-mode nil)
+  (add-hook 'web-mode-hook 'web-mode-init)
+  :custom
+  (web-mode-engines-alist
+   '(("django" . "\\.html\\'")))
+
+  ;; Disable auto-quoting
+  (web-mode-enable-auto-quoting nil)
+  (web-mode-markup-indent-offset config-indent-web-mode-spaces)
+  (web-mode-css-indent-offset config-indent-web-mode-spaces)
+  (web-mode-code-indent-offset config-indent-web-mode-spaces)
+  ;; Don't lineup element attributes
+  (web-mode-attr-indent-offset config-indent-web-mode-spaces)
+  ;; Automatically close tag
+  (web-mode-enable-auto-pairing t)
+  (web-mode-enable-css-colorization t)
+  :hook (web-mode . (lambda ()
+                      (when (string-equal "vue" (file-name-extension buffer-file-name))
+                        (setup-vue-mode)))))
+
 (use-package css-mode
   :hook (css-mode . enable-prettier-js-mode))
 
